c0132617785d3ff5a45bb170f4000b83e16c8d72
larissabndct <larissagomes0119@gmail.com>
2024-05-02 15:11:42 -0300

    teste push por vs code

---
+++ Models/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoDotnet.Models
+{
+    public class Pessoa
+    {
+        public int idade { get; set; }
+        public string nome { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine("Olá! Meu nome é "+ nome +", e tenho " + idade + " anos de idade");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using cursoDotnet.Models;
+
+Pessoa pessoa= new Pessoa();
+
+pessoa.nome="Larissa";
+pessoa.idade=22;
+pessoa.Apresentar();
